03248f779a3da22477400e157775bb3e130ac0fb
Scarecrowy2022 <116049072+Scarecrowy2022@users.noreply.github.com>
2023-09-13 14:11:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #===============================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+sed -i "s/OpenWrt /Yang J build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
